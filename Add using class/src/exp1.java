aaac24b338d584d58c77ec6a274836c3025b1aec
Nurilhijas <40590317+Nurilhijas@users.noreply.github.com>
2018-08-12 23:02:20 +0530

    Add files via upload

---
+++ Add using class/src/exp1.java
@@ -0,0 +1,25 @@
+
+/**
+ * 
+ */
+
+/**
+ * @author Nuril Hijas
+ *
+ */
+public class exp1 {
+	/**
+	 * @param args
+	 */
+void calculate()
+{
+	
+	System.out.println(10+20);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+exp1 r1 =new exp1();
+r1.calculate();
+	}
+
+}
