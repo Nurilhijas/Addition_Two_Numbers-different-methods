b3efb8773a6e17cc6023bb903f2182b5915e2d4e
Nurilhijas <40590317+Nurilhijas@users.noreply.github.com>
2018-08-12 23:19:43 +0530

    Add files via upload

---
+++ mutithreading/src/mutithreading/adding.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package mutithreading;
+
+/**
+ * @author Nuril Hijas
+ *
+ */
+class adding implements Runnable{
+	  
+	  int a,b; 
+	   
+	 public adding(int a, int b){
+	 
+	       this.a = a;
+	  
+	      this.b = b;
+	  
+	   }
+	    
+	public void run() {
+	      
+	  add(a,b);
+	  
+	  }
+	   
+	 public void add(int a, int b){
+	   
+	     int sum=0;
+	      
+	  synchronized (this) {
+	        
+	    for(int i=a;i<=b;i++){
+	         
+	       sum = sum+ a;
+	        
+	    }
+	       
+	     System.out.println("Sum of "+a+" to "+ b+" numbers = "+sum);    
+	 
+	       }
+	  
+	  }
+	
+
+
+	public class addnumbersusing5threads
+	{
+	  
+	  public void main(String[] args) {
+	     
+	   Thread t1 = new Thread(new adding(1,10));
+	     
+	        
+	t1.start();
+	      
+	
+	    }
+
+	}
+
+}
